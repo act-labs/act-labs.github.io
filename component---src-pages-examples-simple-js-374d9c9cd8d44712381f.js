d563490b553d4b906b83f2e0c732be8baa8190c2
eugeny2b@gmail.com <eugeny2b@gmail.com>
2019-08-01 23:30:10 +0100

    Updates

---
+++ component---src-pages-examples-simple-js-374d9c9cd8d44712381f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[41],{451:function(e,n,t){"use strict";t.r(n);var l=t(0),a=t.n(l);n.default=function(){return a.a.createElement("div",null,a.a.createElement("h1",null,"The simplest page possible"),a.a.createElement("p",null,"Just for testing."))}}}]);
+//# sourceMappingURL=component---src-pages-examples-simple-js-374d9c9cd8d44712381f.js.map
